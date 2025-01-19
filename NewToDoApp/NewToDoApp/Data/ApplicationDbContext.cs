2fa98d0a3183360eac046c9a208d6e0223d3b505
shalant <doug.rosenberg@gmail.com>
2025-01-18 22:03:20 -0600

    Adding basic structure

--- NewToDoApp/NewToDoApp/Data/ApplicationDbContext.cs
+++ NewToDoApp/NewToDoApp/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NewToDoApp.Models;
 
 namespace NewToDoApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
     }
 }
---
+++ Todo.Models/ToDoItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewToDoApp.Models
+{
+    public class ToDoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
