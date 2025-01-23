59b37d1060d86f38177d96c14da1c83af23f0100
shalant <doug.rosenberg@gmail.com>
2025-01-22 20:26:31 -0600

    added muddy toast to the change state checkbox

--- NewToDoApp/NewToDoApp.Client/Program.cs
+++ NewToDoApp/NewToDoApp.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using NewToDoApp.Client;
 using NewToDoApp.Client.Services;
@@ -7,8 +8,18 @@ using NewToDoApp.Client.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
 
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 4000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 builder.Services.AddScoped<IToDoService, ToDoService>();
 
 builder.Services.AddAuthorizationCore();
